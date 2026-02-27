5c2ae7b1047e9fd086d9bbcda92005717068e3cb
XiaoNiao <unsignedintegers@gmail.com>
2026-02-27 22:10:04 +0800

    Add files via upload

---
+++ languages/chinese.php
@@ -0,0 +1,24 @@
+<?php
+// The friendly name of the language
+$langinfo['name'] = "中文 (简体)";
+
+// The author of the language
+$langinfo['author'] = "小鸟";
+
+// The language authors website
+$langinfo['website'] = "https://mybb.com/";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1839";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 1;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "zh";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
